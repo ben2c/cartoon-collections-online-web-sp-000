b442c78fdae790b93cdda71609ca4c49c86b99a2
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 03:40:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.find |i| i.length > 4
+  if array.find |i| i.length > 4
     return true
   else
     false

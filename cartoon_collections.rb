c2256a2650f59a964e0a3b2e48757f62b0bbd4f3
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 03:46:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?("cheddar" || "gouda" || "camembert")
+  array.include("cheddar" || "gouda" || "camembert")
 
 end

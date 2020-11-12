770e6d34f9e6dfb6946311647f0641922a25988b
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 03:35:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |element|
+    element.capitalize +"!"
+  end
 end
 
 def long_planeteer_calls# code an argument here

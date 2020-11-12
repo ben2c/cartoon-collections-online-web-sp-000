2e2348fcbcdc353c03ec9a223633e0e014707bec
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 03:40:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,12 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  if array.find |i| i.length > 4
+    return true
+  else
+    false
 end
 
 def find_the_cheese# code an argument here
